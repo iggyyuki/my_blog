077919b2cd6984656f7b6554aaa5507118b09504
Yuki <tigers24.backfrip.yuki1117@gmail.com>
2016-11-27 07:41:40 +0000

    route作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  # get 'posts/index' 
 
-  get 'posts/show'
+  # get 'posts/show'
 
-  get 'posts/new'
+  # get 'posts/new'
 
-  get 'posts/create'
+  # get 'posts/create'
 
-  get 'posts/edit'
+  # get 'posts/edit'
 
-  get 'posts/update'
+  # get 'posts/update'
 
-  get 'posts/destroy'
+  # get 'posts/destroy'
+  
+  resources :posts
+  root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
